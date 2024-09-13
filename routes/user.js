57cb9182853cf16e9ee9ad0a8fc9e753a86a0875
aayush patidar <ap6887980@gmail.com>
2024-09-13 22:08:04 +0530

    restructure our code , we have implmented MVC in listing, user and review

--- routes/user.js
+++ routes/user.js
@@ -5,59 +5,26 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
 
-router.get("/signup", async (req, res) => {
-  await res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users");
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
-);
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl, // Middleware to save redirect URL
+    passport.authenticate("local", {
+      // Passport authentication middleware
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
 
-router.post(
-  "/login",
-  saveRedirectUrl, // Middleware to save redirect URL
-  passport.authenticate("local", {
-    // Passport authentication middleware
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  async (req, res) => {
-    req.flash("success", "Welcome back to Wanderlust!");
-    let redirectUrl = res.locals.redirectUrl || "/listings"; // Get redirect URL or default to /listings
-    res.redirect(redirectUrl); // Redirect to the saved URL or default URL
-  }
-);
-
-router.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "you are logged out!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
